993e1f5798f14bdbac31c54a4a7dfcf128d5e9e6
Jesús Terán <64324782+teranjc@users.noreply.github.com>
2023-06-27 17:18:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,10 @@ app.add_middleware(
 async def index(request: Request):
     return {"hello word"}
 
-
 load_dotenv()
 app.include_router(auth_route, prefix="/api")
 app.include_router(license_route, prefix="/api")
 app.include_router(user_route, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="licenses-back.vercel.app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
